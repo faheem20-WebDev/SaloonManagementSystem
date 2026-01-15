938f45bfc7f0161d8fb5d502275f74a1e31f9e3f
faheem20-WebDev <muhammadfaheem42004@gmail.com>
2026-01-15 16:39:21 +0500

    Added trailing slash to baseURL for correct routing

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // Direct link for deployment
-  // NOTE: We removed the trailing slash and will remove leading slashes from requests
-  baseURL: 'https://muhammadfaheem52006-saloonmanagementsystembackend.hf.space/api',
+  // Adding the trailing slash is CRITICAL for Axios to join paths correctly
+  baseURL: 'https://muhammadfaheem52006-saloonmanagementsystembackend.hf.space/api/',
 });
 
 // Add a request interceptor
